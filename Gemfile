814d5eaaed4a517983f8d9774c113754a50ae9d2
Nour Souid <noursouidd@gmail.com>
2020-03-24 13:46:14 -0400

    added gem in gemfile

--- Gemfile
+++ Gemfile
@@ -1,8 +1,7 @@
 source 'https://rubygems.org'
 ruby '2.6.5'
 
-gem 'rails-assets-particles.js'
-
+gem 'particles-js-rails'
 gem 'bootsnap', require: false
 gem 'jbuilder', '~> 2.0'
 gem 'pg', '~> 0.21'
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,8 @@
 import "bootstrap";
 
-/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+// particlesJS.load(@dom-id, @path-json, @callback (optional));
+
+
 particlesJS.load('particles-js', 'assets/particles.json', function() {
   console.log('callback - particles.js config loaded');
 });
