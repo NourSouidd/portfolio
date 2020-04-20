9ebd2e8c4fcdb6339121ba3dcdeaee004f02b895
Nour Souid <noursouidd@gmail.com>
2020-04-20 18:58:52 -0400

    added carousel

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,8 +1,27 @@
 import "bootstrap";
+// import initUpdateNavbarOnScroll from "./navbar.js";
 
 // particlesJS.load(@dom-id, @path-json, @callback (optional));
 
+const initUpdateNavbarOnScroll = () => {
+  console.log('heyyy');
+  const navbar = document.querySelector('.navbar-portfolio');
+  console.log(navbar);
+  if (navbar) {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY >= window.innerHeight) {
+        navbar.classList.add('navbar-purple');
+      } else {
+        navbar.classList.remove('navbar-purple');
+      }
+    });
+  }
+}
 
-particlesJS.load('particles-js', 'assets/particles.json', function() {
-  console.log('callback - particles.js config loaded');
+document.addEventListener('turbolinks:load', () => {
+  initUpdateNavbarOnScroll();
 });
+
+// particlesJS.load('particles-js', 'assets/particles.json', function() {
+//   console.log('callback - particles.js config loaded');
+// });
