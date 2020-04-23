8eb2d6aad9c6d036a2491b54960ed692519972cc
Nour Souid <noursouidd@gmail.com>
2020-04-23 01:56:13 -0400

    fixed link

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,24 +2,24 @@ import "bootstrap";
 // import initUpdateNavbarOnScroll from "./navbar.js";
 
 // particlesJS.load(@dom-id, @path-json, @callback (optional));
-document.addEventListener('turbolinks:load', () => {
-  console.log('heyyy');
-  initUpdateNavbarOnScroll();
-});
+// document.addEventListener('turbolinks:load', () => {
+//   console.log('heyyy');
+//   initUpdateNavbarOnScroll();
+// });
 
-const initUpdateNavbarOnScroll = () => {
-  const navbar = document.querySelector('.navbar-portfolio');
-  console.log(navbar);
-  if (navbar) {
-    window.addEventListener('scroll', () => {
-      if (window.scrollY >= window.innerHeight) {
-        navbar.classList.add('navbar-purple');
-      } else {
-        navbar.classList.remove('navbar-purple');
-      }
-    });
-  }
-}
+// const initUpdateNavbarOnScroll = () => {
+//   const navbar = document.querySelector('.navbar-portfolio');
+//   console.log(navbar);
+//   if (navbar) {
+//     window.addEventListener('scroll', () => {
+//       if (window.scrollY >= window.innerHeight) {
+//         navbar.classList.add('navbar-purple');
+//       } else {
+//         navbar.classList.remove('navbar-purple');
+//       }
+//     });
+//   }
+// }
 
 
 // particlesJS.load('particles-js', 'assets/particles.json', function() {
