7a839f44a12abcccf6fb5ba03a04ff6d70e155a2
Nour Souid <noursouidd@gmail.com>
2020-04-25 15:36:01 -0400

    fixed the links to the bottom

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,27 +1,20 @@
 import "bootstrap";
-// import initUpdateNavbarOnScroll from "./navbar.js";
-
-// particlesJS.load(@dom-id, @path-json, @callback (optional));
-// document.addEventListener('turbolinks:load', () => {
-//   console.log('heyyy');
-//   initUpdateNavbarOnScroll();
-// });
-
-// const initUpdateNavbarOnScroll = () => {
-//   const navbar = document.querySelector('.navbar-portfolio');
-//   console.log(navbar);
-//   if (navbar) {
-//     window.addEventListener('scroll', () => {
-//       if (window.scrollY >= window.innerHeight) {
-//         navbar.classList.add('navbar-purple');
-//       } else {
-//         navbar.classList.remove('navbar-purple');
-//       }
-//     });
-//   }
-// }
 
 
-// particlesJS.load('particles-js', 'assets/particles.json', function() {
-//   console.log('callback - particles.js config loaded');
-// });
+const initUpdateNavbarOnScroll = () => {
+  const navbar = document.querySelector('.navbar-portfolio');
+  console.log("heyy");
+  if (navbar) {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY >= window.innerHeight) {
+        navbar.classList.add('navbar-purple');
+      } else {
+        navbar.classList.remove('navbar-purple');
+      }
+    });
+  }
+}
+
+document.addEventListener('turbolinks:load', () => {
+  initUpdateNavbarOnScroll();
+});
